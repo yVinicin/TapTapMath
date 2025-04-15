52d90a2f5b6aae28758002ddd21c67e46a2ab006
Vinicin <viniciusmmarcos20@gmail.com>
2025-04-15 13:23:25 -0300

    Criação inicial da tela do jogo

--- src/Screens/GameScreen.js
+++ src/Screens/GameScreen.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome'
 
-export function GameScreen() {
+export function GameScreen({ navigation }) {
   return (
-    <View>
-      <Text>Aqui é a GameScreen!</Text>
+    <View style={styles.container}>
+
+    <View style={styles.ArrowContainer}>
+      <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+        <Icon name="arrow-left" size={20} style={styles.Arrow} />
+      </TouchableOpacity>
+    </View>
+
     </View>
   );
 }
+
+let styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgb(0, 0, 0)',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: '20%',
+  },
+  ArrowContainer: {
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    marginRight: '80%'
+  },
+  Arrow: {
+    color: 'rgb(0, 0, 0)',
+    backgroundColor: 'rgb(255, 255, 255)',
+    borderRadius: 20,
+    paddingHorizontal: 5,
+    paddingVertical: 3,
+  },
+});
