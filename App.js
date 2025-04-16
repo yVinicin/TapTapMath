f2cc99526ea1285031d9d80ec18f1ce3e803a415
Vinicin <viniciusmmarcos20@gmail.com>
2025-04-16 17:24:38 -0300

    Remoção da Barra de Status dentro do aplicativo

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from './src/Screens/HomeScreen';
@@ -9,11 +10,14 @@ const Stack = createNativeStackNavigator();
 // Controlador entre as telas do jogo
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Game" component={GameScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <StatusBar hidden={true} />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Game" component={GameScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
